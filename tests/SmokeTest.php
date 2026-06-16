c6a7eccf40e9533c7021b7fd31b8651cb888ccbf
VoiceTel <support@voicetel.com>
2026-06-16 16:56:58 -0400

    test: bump smoke version pin to 0.7.1 (couples with runtime constant)

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -798,9 +798,9 @@ final class SmokeTest extends TestCase
         self::assertSame('cursor-abc123', $query['PageToken']);
     }
 
-    public function testVersionIs070(): void
+    public function testVersionIs071(): void
     {
-        self::assertSame('0.7.0', Version::VERSION);
+        self::assertSame('0.7.1', Version::VERSION);
     }
 
     public function testCreateParticipantSendsFromAndTo(): void
